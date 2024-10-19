f836dfc47efb8d14464b7fe1af9a7c8df9ed4dd3
Silas Carmo <augusto.fernandes775@gmail.com>
2024-10-19 00:17:11 -0300

    fix: add standard user permission

--- src/database/migrations/1729208964205-Seeds.ts
+++ src/database/migrations/1729208964205-Seeds.ts
@@ -7,7 +7,7 @@ export class Seeds1729208964205 implements MigrationInterface {
         VALUES (1, 'Company 1', 'Address Company 1', '99999999999');
 
         INSERT INTO users (company_id, name, login, password, roles)
-        VALUES (1, 'User Admin 1', 'user1_admin', '$2a$10$eLWMvmkruQxv59P1N8CGBeIzDgtEzqyJrY7K1DfLcZ1GuQmIEhjAG', '{ADMIN}')
+        VALUES (1, 'User Admin 1', 'user1_admin', '$2a$10$eLWMvmkruQxv59P1N8CGBeIzDgtEzqyJrY7K1DfLcZ1GuQmIEhjAG', '{MAIN,ADMIN}')
     `);
   }
 
